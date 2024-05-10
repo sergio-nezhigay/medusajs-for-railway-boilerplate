4552d2f61b2f8cbd416cde9356d8a34825fcbd6c
Sergio Nezhigay <abc1971h@gmail.com>
2024-05-10 09:59:20 +0300

    options change

--- medusajs-backend/medusa-config.js
+++ medusajs-backend/medusa-config.js
@@ -79,6 +79,8 @@ const plugins = [
       api_key: process.env.SENDGRID_API_KEY,
       from: process.env.SENDGRID_FROM,
       order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID,
+      customer_password_reset_template: process.env.SENDGRID_ORDER_PLACED_ID,
+      user_password_reset_template: process.env.SENDGRID_ORDER_PLACED_ID,
     },
   },
 ];
