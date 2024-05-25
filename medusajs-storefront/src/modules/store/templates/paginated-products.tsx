fa7a5eaef6cc69f79188e050daed471869028112
Sergio Nezhigay <abc1971h@gmail.com>
2024-05-25 13:51:41 +0300

    increrease limit

--- medusajs-storefront/src/modules/store/templates/paginated-products.tsx
+++ medusajs-storefront/src/modules/store/templates/paginated-products.tsx
@@ -3,7 +3,7 @@ import ProductPreview from "@modules/products/components/product-preview"
 import { Pagination } from "@modules/store/components/pagination"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 
-const PRODUCT_LIMIT = 2
+const PRODUCT_LIMIT = 10
 
 type PaginatedProductsParams = {
   limit: number
